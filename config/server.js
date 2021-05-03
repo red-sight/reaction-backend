12e8d4c99319a0d5bc987797e7fa617c4564e413
dm112 <dm112@tadbox.com>
2021-05-03 17:38:22 +0300

    ci

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("", "SERVER_URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "d6f5f3c5e90f28551c15f4c93caa3d8d")
